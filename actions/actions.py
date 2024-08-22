d7f5e68d2e2d9eee970890f90b4bc7295c9e3a12
AIAnupSHrestha <shresthanup886@gmail.com>
2024-08-22 18:43:47 +0545

    made gpt relevancy pipeline

--- actions/actions.py
+++ actions/actions.py
@@ -13,13 +13,17 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet, Form, FollowupAction
 from rasa_sdk import Action, FormValidationAction, forms
+
 import json
+
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+
 import re
 from fpdf import FPDF
 
+import ollama
 from reportlab.pdfgen import canvas 
 from reportlab.pdfbase.ttfonts import TTFont 
 from reportlab.pdfbase import pdfmetrics 
@@ -29,7 +33,7 @@ from reportlab.lib import colors
 # from mysql.connector import errorcode
 
 load_dotenv()
-OPENAI_KEY = "123456787654321" #os.getenv("OPENAI_API_KEY")
+OPENAI_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(
   api_key=OPENAI_KEY
 )
@@ -183,19 +187,19 @@ class ActionPolicyType(Action):
                 dispatcher.utter_message(text="No policy type selected")
             return []
 
-def prompt_engineering(prompt):
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "user", "content": prompt},
-        ],
-    )
-    return response.choices[0].message.content
-    response = client.completions.create(
-        model="gpt-3.5-turbo-instruct",
-        prompt=prompt,
-    )
-    return response.choices[0].text
+# def prompt_engineering(prompt):
+#     response = client.chat.completions.create(
+#         model="gpt-4o",
+#         messages=[
+#             {"role": "user", "content": prompt},
+#         ],
+#     )
+#     return response.choices[0].message.content
+#     response = client.completions.create(
+#         model="gpt-3.5-turbo-instruct",
+#         prompt=prompt,
+#     )
+#     return response.choices[0].text
 
 class ActionSelectFlexibleWorkOption(Action):
 
@@ -276,120 +280,42 @@ class ActionGetQuestions(Action):
 #             "response8": self.from_text(),
 #             "response9": self.from_text()
 #         }
+def prompt_engineering(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+    )
+    return response.choices[0].message.content
+
+yes_no_prompt = """
+    Is the following answer relevant to the question {current_question}: '{user_answer}'? Answer with 'yes' or 'no' OR 'true' or 'false', without any additional explanation. .
+    """
 
 class ValidateQuestionForm(FormValidationAction):
     def name(self):
         return "validate_question_form"
+    
 
     def validate_response0(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-        response = tracker.get_slot("response0")
+        current_question = tracker.get_slot("question0")
+        user_answer = tracker.latest_message.get('text')
+        index = tracker.get_slot("question_index")
 
-        if response:
-            return {"response0": response}
+        prompt = yes_no_prompt.format(current_question=current_question, user_answer=user_answer)
+
+        print(prompt)
+
+        answer_relevance = prompt_engineering(prompt=prompt).lower()
+        
+        if answer_relevance == "yes":
+            return {"response0": user_answer}
         else:
-            dispatcher.utter_message(text="Please enter a valid value.")
-            return {"response0": None}
+            dispatcher.utter_message(text="Please enter a answer relevant to the question.")
+            return {"response0": None,
+                    "question_index": index - 1}
     
-    # def validate_response1(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     response = tracker.get_slot("response1")
-
-    #     if response:
-    #         return {"response1": response}
-    #     else:
-    #         dispatcher.utter_message(text="Please enter a valid value.")
-    #         return {"response1": None}
-    
-    # def validate_response2(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     response = tracker.get_slot("response2")
-
-    #     if response:
-    #         return {"response2": response}
-    #     else:
-    #         dispatcher.utter_message(text="Please enter a valid value.")
-    #         return {"response2": None}
-    
-    # def validate_response3(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     response = tracker.get_slot("response3")
-
-    #     if response:
-    #         return {"response3": response}
-    #     else:
-    #         dispatcher.utter_message(text="Please enter a valid value.")
-    #         return {"response3": None}
-    
-    # def validate_response4(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     response = tracker.get_slot("response4")
-
-    #     if response:
-    #         return {"response4": response}
-    #     else:
-    #         dispatcher.utter_message(text="Please enter a valid value.")
-    #         return {"response4": None}
-    
-    # def validate_response5(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     response = tracker.get_slot("response5")
-
-    #     if response:
-    #         return {"response5": response}
-    #     else:
-    #         dispatcher.utter_message(text="Please enter a valid value.")
-    #         return {"response5": None}
-        
-    # def validate_response6(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     response = tracker.get_slot("response6")
-
-    #     if response:
-    #         return {"response6": response}
-    #     else:
-    #         dispatcher.utter_message(text="Please enter a valid value.")
-    #         return {"response6": None}
-    
-    # def validate_response7(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     response = tracker.get_slot("response7")
-
-    #     if response:
-    #         return {"response7": response}
-    #     else:
-    #         dispatcher.utter_message(text="Please enter a valid value.")
-    #         return {"response7": None}
-        
-    # def validate_response8(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     response = tracker.get_slot("response8")
-
-    #     if response:
-    #         return {"response8": response}
-    #     else:
-    #         dispatcher.utter_message(text="Please enter a valid value.")
-    #         return {"response8": None}
-    
-    # def validate_response9(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     response = tracker.get_slot("response9")
-
-    #     if response:
-    #         return {"response9": response}
-    #     else:
-    #         dispatcher.utter_message(text="Please enter a valid value.")
-    #         return {"response9": None}
-    
-
-    # def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-    #     questions = flexible_questions["question"]
-    #     question_index = tracker.get_slot("question_index") or 0
-    #     responses = tracker.get_slot("user_response") or []
-
-    #     if question_index < len(questions):
-    #         current_question = questions[question_index]
-    #         dispatcher.utter_message(text=current_question)
-            
-    #         question_index += 1
-            
-    #         return [SlotSet("question_index", question_index),
-    #                 SlotSet("user_response", None)]
-    #     else:
-    #         dispatcher.utter_message(text="Thank you for answering all the questions!")
-            
-    #         return [SlotSet("question_index", None)]
-        
 
 
 class ActionStoreResponse(Action):
@@ -409,9 +335,6 @@ class ActionStoreResponse(Action):
         pdf.output("Policy_Questions_and_Responses.pdf")
          
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict):
-        # for num in range(10):
-            # user_response = tracker.get_slot(f"response{num}")
-            # response.append(user_response)
         dispatcher.utter_message(text="Your responses are recorded")
         response = tracker.get_slot("response")
         self.save_pdf(response)
@@ -455,63 +378,13 @@ class ActionActivateForm(Action):
     
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:        
         return [Form("question_form")]#,
-                # FollowupAction("action_custom_fallback")]
-    
-        # return [SlotSet("question0", question_list[0]),
-        #         SlotSet("question1", question_list[1]),
-        #         SlotSet("question2", question_list[2]),
-        #         SlotSet("question3", question_list[3]),
-        #         SlotSet("question4", question_list[4]),
-        #         SlotSet("question5", question_list[5]),
-        #         SlotSet("question6", question_list[6]),
-        #         SlotSet("question7", question_list[7]),
-        #         SlotSet("question8", question_list[8]),
-        #         SlotSet("question9", question_list[9]),
-        #         Form("question_form")]
+
     
 class actionCustomFallback(Action):
     def name(self):
         return "action_custom_fallback"       
         
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any])  -> List[Dict[Text, Any]]:
-        # prompt = """You are a helpful, trustworthy and informative medical assistant named 'Angela'. Please follow the following guidelines:
-        #                 1. Angela should respond users query helping user to understand their health concerns and navigate users towards appropriate resources. 
-        #                 2. Angela can provide general health information and symptom assessment, Angela cannot diagnose illnesses or prescribe medications.
-        #                 3. Angela should reply in a short and precise content imitating real human-like conversations.
-        #                 4. Angela emphasizes the importance of consulting a doctor or pharmacist for medication advice.
-        #                 5. Angela avoids providing specific dosages to prevent misuse.
-        #                 6. Angela can offer general information about side effects while directing the user to reliable sources like medication leaflets and healthcare professionals. 
-        #                 7. Angela can not assist with any non-medical query. 
-        #                 8. Angela should only give relevant information only.
-        #          """
-
-        # try:
-        #     user_query = tracker.latest_message['text']
-        #     response = client.chat.completions.create(
-        #         model = "gpt-4o",
-        #         messages=[
-        #             {"role": "system", "content": prompt},
-        #             {"role": "user", "content": "What's the best medication for my cough?"},
-        #             {"role": "assistant", "content": "I understand you're looking for relief from your cough. While I can't recommend specific medications, I suggest consulting a doctor if your cough persists."},
-        #             {"role": "user","content": user_query}
-        #         ]
-        #     )
-        #     data = response.choices[0].message.content
-        #     attachment = {
-		#     	"query_response": data,
-		#     	"data":[],
-		#     	"type":"normal_message",
-		#     	"data_fetch_status": "success"
-		#     }
-        # except Exception as e:
-        #     data = "Can you rephrase it."
-        #     attachment = {
-		#     	"query_response": data,
-		#     	"data":[],
-		#     	"type":"normal_message",
-		#     	"data_fetch_status": "failed"
-		#     }
-        # dispatcher.utter_message(attachment=attachment)
         user_response = tracker.latest_message['text']
 
         if tracker.get_slot("response"):
